24e90b1eddf33a7ac425b47ddd3a736980b7dd76
buttery-potato <71145077+buttery-potato@users.noreply.github.com>
2020-12-02 09:25:28 -0800

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,4 +1,5 @@
-sudo pacman -S code ufw flat-remix flat-remix-gtk flat-remix-gnome flameshot 
+sudo pacman -S code ufw flat-remix flat-remix-gtk flat-remix-gnome flameshot gnome-shell-extension-dash-to-panel
 sudo ufw enable
 sudo ufw default deny incoming
 sudo ufw default allow outgoing
+echo "Remember to install \"User Themes\"!"
