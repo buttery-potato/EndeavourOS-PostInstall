0e3284ee1048dead9174397045f62f5a0f2eeece
buttery-potato <71145077+buttery-potato@users.noreply.github.com>
2020-12-03 13:17:08 -0800

    Update script.sh

--- script.sh
+++ script.sh
@@ -2,4 +2,4 @@ yay -S code ufw moe-kde-git latte-dock flameshot fish pkgfile
 sudo ufw enable
 sudo ufw default deny incoming
 sudo ufw default allow outgoing
-echo "Remember to install \"User Themes\"!"
+
